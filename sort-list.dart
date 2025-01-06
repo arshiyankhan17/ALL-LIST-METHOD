98959d631204db9bfd29436c1d0c210ba0c7a0c0
arshiyankhan17 <arshiyanahmedkhan17@gmail.com>
2025-01-06 10:38:36 +0500

    sort list method

---
+++ sort-list.dart
@@ -0,0 +1,8 @@
+void main() {
+var intList = [0, 5, 2, 3, 8, 17, 11];
+intList.sort();
+print(intList);
+var tringList = ['vue', 'kotlin','dart', 'angular', 'flutter'];
+tringList.sort();
+print(tringList);
+}

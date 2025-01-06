addeb114f826c40718622ad3f76af17ba6a9949e
arshiyankhan17 <arshiyanahmedkhan17@gmail.com>
2025-01-06 09:58:22 +0500

    method l.first , l.last , l.elementAt() ,l.length

---
+++ index.dart
@@ -0,0 +1,18 @@
+void main() {
+ 
+ var list = [1, 2, 3, 4, 5];
+ var e1 = list.first;
+ var e2 = list.last;
+ var e3 = list.elementAt(1);
+ var len = list.length;
+ 
+ print('There are $len elements in the list');
+ print('The first element is $e1');
+ print('The last element is $e2');
+ print('The second element is $e3');
+}
+
+// is method ko dekhne se pata chalta hai ?  
+// list.first yah phela variable la degah .
+// list.last yah bhi last variable la degah .
+// list.length jatni list ki lenght hogi yah sari print kar de gah jese is list ki lenght [1 2 3 4 5] hai tu yah 5 print kar gah .  

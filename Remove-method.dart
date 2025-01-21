60fbaa791e7a46678fa7401204eb49d4fabc9565
arshiyankhan17 <arshiyanahmedkhan17@gmail.com>
2025-01-21 16:09:47 +0500

    For Each

--- Remove-method.dart
+++ Remove-method.dart
@@ -1,4 +1,5 @@
  void main() {
+ //Example........
  var vals2 = [-2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   vals2.removeWhere((e) => e < 0);
   print(vals2);
@@ -7,3 +8,7 @@
   vals2.retainWhere((e) => e > 7);
   print(vals2);
  }
+
+//.removeWhere index ki badolat remove karta hai...
+//.removeRange ak range lekar remove karta hai or range aese leta hai (index , index) jaha se remove karna hai uska index or jaha tak karna hai uska index..
+//.retainWhere koi bhi index nahi leta wo leta hai element jo exists karta hu jese maine example ma liya "7" tu iska output aya yah [8,9,10].   
---
+++ forEach.dart
@@ -0,0 +1,7 @@
+void main (){
+
+var list = [3 ,4 ,5 , 6 , 7 , 8];
+
+  list.forEach(print);
+
+}
